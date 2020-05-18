f02f69f9060cc1f289099132f214f00f4734b6ed
Hans Melby <hanshmelby@gmail.com>
2020-05-18 21:14:09 +0200

    Code for finding name, price, and unit price for product added

--- kolonial_webscraper.py
+++ kolonial_webscraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup as Soup
 import requests
+from datetime import date
 
 
 def get_html_doc(url):
@@ -22,15 +23,28 @@ def get_all_product_category_links(url):
     return product_category_links
 
 
-
-links = get_all_product_category_links("https://kolonial.no/produkter/")
-
-for link in links:
-    print(link)
-
-
 def get_all_products_from_category(url):
-    prooduct_categories =
+    r = get_html_doc(url)
+    r.raise_for_status
+
+
+#print_html_doc_body("https://kolonial.no/produkter/salg/")
+category_product_soup = get_html_doc("https://kolonial.no/produkter/salg/")
+products = category_product_soup.find_all("div", {"class": "product-list-item"})
+
+#print(products[0])
+
+name = products[0].find("div", {"class": "name-main"})
+price = products[0].find("p", {"class": "price label label-price"})
+unit_price = products[0].find("p", {"class": "unit-price"})
+
+
+print(name.text.strip())
+print(price.text.strip())
+print(unit_price.text.strip())
+print(datetime.date.today.)
+
+#print(products[0].p[class"price label label-price"])
 
 #product_categories = get_all_product_categories("https://kolonial.no/produkter/")
 #print(product_categories)
