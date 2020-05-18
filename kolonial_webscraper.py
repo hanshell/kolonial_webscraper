096b68208fbd8525974a3d115a28a486ef860dc1
Hans Melby <hanshmelby@gmail.com>
2020-05-18 22:14:48 +0200

    Product info extraction working

--- kolonial_webscraper.py
+++ kolonial_webscraper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup as Soup
 import requests
-from datetime import date
+import time
+import re
 
 
 def get_html_doc(url):
@@ -23,26 +24,50 @@ def get_all_product_category_links(url):
     return product_category_links
 
 
+# Get name, price, and weight information from product
+def find_product_info(product_soup):
+    name = product_soup.find("div", {"class": "name-main"})
+    price = 0
+    if "label-price-discounted" in str(product_soup):
+        price = product_soup.find("span", {"class": "undiscounted-price"})
+    else:
+        price = product_soup.find("p", {"class": "price label label-price"})
+    unit_price = product_soup.find("p", {"class": "unit-price"})
+    return name, price, unit_price
+
+
 def get_all_products_from_category(url):
     r = get_html_doc(url)
     r.raise_for_status
 
+    category_product_soup = get_html_doc(url)
+    products = category_product_soup.find_all("div", {"class": "product-list-item"})
+
+    for product in products:
+        name, price, unit_price = find_product_info(product)
+
+        print(name.text.strip())
+        print(price.text.strip())
+        print(unit_price.text.strip())
+
+
+get_all_products_from_category("https://kolonial.no/produkter/salg/")
 
 #print_html_doc_body("https://kolonial.no/produkter/salg/")
-category_product_soup = get_html_doc("https://kolonial.no/produkter/salg/")
-products = category_product_soup.find_all("div", {"class": "product-list-item"})
+#category_product_soup = get_html_doc("https://kolonial.no/produkter/salg/")
+#products = category_product_soup.find_all("div", {"class": "product-list-item"})
 
 #print(products[0])
 
-name = products[0].find("div", {"class": "name-main"})
-price = products[0].find("p", {"class": "price label label-price"})
-unit_price = products[0].find("p", {"class": "unit-price"})
+#name = products[0].find("div", {"class": "name-main"})
+#price = products[0].find("p", {"class": "price label label-price"})
+#unit_price = products[0].find("p", {"class": "unit-price"})
 
 
-print(name.text.strip())
-print(price.text.strip())
-print(unit_price.text.strip())
-print(datetime.date.today.)
+#print(name.text.strip())
+#print(price.text.strip())
+#print(unit_price.text.strip())
+#print(time.asctime(time.localtime(time.time())))
 
 #print(products[0].p[class"price label label-price"])
 
